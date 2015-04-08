7707a564f398b8efef9aa8d2212adfeb685667f8
Charles-André Després <cadespres@outlook.com>
2015-04-09 01:01:16 +0200

    Initial commit Spring project

---
+++ src/main/java/com/lanouvellediete/backoffice/Application.java
@@ -0,0 +1,17 @@
+package com.lanouvellediete.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by cadespres on 18/03/15.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
